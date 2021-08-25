9c5672ef9c3ecc45acec7967535f2970e13f697a
Jung Jongmin <sdr2936@gmail.com>
2021-08-25 19:53:15 +0900

    Test

--- 02_JPA/src/main/java/hellojpa/JpaMain.java
+++ 02_JPA/src/main/java/hellojpa/JpaMain.java
@@ -2,6 +2,7 @@ package hellojpa;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -9,26 +10,36 @@ public class JpaMain {
         EntityManager em=emf.createEntityManager();
         em.setFlushMode(FlushModeType.AUTO);
         EntityTransaction tx=em.getTransaction();
+        Member member=null;
+        Member member2=null;
+        System.out.println("==========tx1 start============");
+        //tx1
         tx.begin();
         try{
-            Member member=new Member();
-            member.setCreatedBy("JJM");
-            member.setCreatedDate(LocalDateTime.now());
-            member.setLastModifiedBy("JJM");
-            member.setLastModifiedDate(LocalDateTime.now());
+            member= (Member) em.find(Member.class,1L);
+            member.setCreatedBy("Jjm");
+            System.out.println("==========경계선. 이밑으로 select나가면 안됨============");
+            member2= (Member) em.find(Member.class,1L);
             em.persist(member);
-            Movie movie=new Movie();
-            movie.setDirector("aaa");
-            movie.setActor("bbb");
-            movie.setName("바람과함께사라지다");
-            movie.setPrice(1000);
-            em.persist(movie);
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
-        } finally {
-            em.close();
         }
+        System.out.println("==========tx1 end============");
+        System.out.println("==========tx2 start============");
+
+        //tx2
+        EntityTransaction tx2=em.getTransaction();
+        tx2.begin();
+        try{
+            member= (Member) em.find(Member.class,1L);
+            System.out.println(member.getCreatedBy());
+        } catch (Exception e) {
+            tx2.rollback();
+        }
+        System.out.println("==========tx2 end============");
+
+        em.close();
         emf.close();
     }
 }
